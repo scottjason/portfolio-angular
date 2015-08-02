04bcb27e53b66d746eaee5c4200057c314e51025
Scott Jason <scottleejason@gmail.com>
2015-08-02 02:44:22 -0700

    adds grunt concat to bower js deps

--- grunt/concat.js
+++ grunt/concat.js
@@ -8,6 +8,10 @@ module.exports = {
     src: ['client/scripts/app.js', 'client/scripts/app.config.js', 'client/scripts/directives/*.js', 'client/scripts/services/*.js', 'client/scripts/controllers/*.js'],
     dest: 'client/build/dev.js'
   },
+  vendor_scripts: {
+    src: ['client/bower_components/angular/angular.min.js', 'client/bower_components/angular-ui-router/release/angular-ui-router.min.js'],
+    dest: 'client/build/vendor.min.js'
+  },
   dev_styles: {
     src: ['client/styles/global.css', 'client/styles/navbar.css', 'client/styles/landing.css', 'client/styles/portfolio.css', 'client/styles/desktop/landing.css', 'client/styles/tablet/landing.css', 'client/styles/mobile/navbar.css', 'client/styles/mobile/landing.css', 'client/styles/mobile/portfolio.css'],
     dest: 'client/build/dev.css'
